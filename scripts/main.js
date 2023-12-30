1b525db3f011ff7173f1902b46451d393fc1f8b8
Akash Rahman <akashrahman.me@gmail.com>
2023-12-30 23:03:45 +0600

    added video popup

--- scripts/main.js
+++ scripts/main.js
@@ -12,3 +12,25 @@ closeButton.addEventListener("click", () => {
   popup.classList.remove("flex");
   popup.classList.add("hidden");
 });
+
+const videos = document.getElementsByClassName("video-button");
+const videoPopup = document.getElementById("video-popup");
+const videoClose = document.getElementById("video-close");
+
+[...videos].map((video) => {
+  video.addEventListener("click", (e) => {
+    e.preventDefault();
+    const videoURL = e.currentTarget.href;
+
+    videoPopup.classList.remove("hidden");
+    videoPopup.classList.add("flex");
+
+    videoPopup.querySelector("iframe").src = videoURL;
+  });
+});
+
+videoClose.addEventListener("click", () => {
+  videoPopup.classList.remove("flex");
+  videoPopup.classList.add("hidden");
+  videoPopup.querySelector("iframe").src = "";
+});
